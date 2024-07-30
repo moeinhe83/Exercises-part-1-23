1175c1130863281f84f794e33c0ef81e6a1848d4
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-07-31 01:19:08 +0330

    Upload Exercises-part-1-23 Project On Github

---
+++ README.md
@@ -0,0 +1,29 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 23
+
+Programming Language ===> Python
+
+Program description ===> 23th Exercise
+
+Example ===> Write A Program That Shows The Difference Between The Following Two Lists
+
+List A => a = [1, 2, 3, 4, 5]
+
+List B => b = [4, 5, 6, 7, 8]
+
+Description Of Files:
+
+practice_23_linux.py ===> For Linux 
+
+For Run ===> python3 practice_23_linux.py
+
+practice_23_windows.py ===> For Windows
+
+For Run ===> python practice_23_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_23_linux.py
@@ -0,0 +1,25 @@
+# Practice_23
+
+# Library
+import random
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 23', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value
+number = range(1, 11)
+# Number Generation
+print(random.choice(number))
+
+# Finish
+# Create By Moein Heshmati
